5b50bf1f009745964d7c3669bad6a07c532c768e
Richard Lewis Jones <richard.lewis.jones@moo.com>
2012-05-29 16:32:48 +0100

    added bling

--- MooExportDialogSections.lua
+++ MooExportDialogSections.lua
@@ -18,61 +18,69 @@ function MooExportDialogSections.sectionsForTopOfDialog(f, propertyTable)
 
    local result = {
       {
-	 title = "MOO Product",
-	 
-	 f:row {
-	    f:static_text {
-	       title = "Make:",
+         title = "MOO",
+         
+         f:row {
+            f:picture {
+	       fill_horizontal = 0.3,
+               value = _PLUGIN:resourceId("moo.png"),
             },
-	    f:popup_menu {
-	       value = bind 'moo_product',
-	       items = {
-		  {
-		     title = 'Business Cards',
-		     value = 'businesscard'
-		  }, 
-		  {
-		     title = 'MiniCards',
-		     value = 'minicard'
-		  }, 
-		  {
-		     title = 'Postcards',
-		     value = 'postcard'
-		  }, 
-		  
-	       }
-	    }
-	 }
-      },
-      {
-	 title = "Orientation",
-	 
-	 f:row {
-	    f:static_text {
-	       title = "Cards will be:",
-            },
-	    f:popup_menu {
-	       value = bind 'moo_orientation',
-	       items = {
-		  {
-		     title = 'Landscape',
-		     value = 'landscape'
-		  }, 
-		  {
-		     title = 'Portrait',
-		     value = 'portrait'
-		  }, 
-		  {
-		     title = 'Oriented Automatically',
-		     value = 'automatic'
-		  }, 
-		  
-	       }
-	    }
+            f:column {
+               spacing = f:control_spacing(),
+               f:row {
+                  spacing = f:label_spacing(),
+                  f:static_text {
+                     title = "Make",
+                     alignment = "right",
+		     width = LrView.share "label_width"
+                  },
+                  f:popup_menu {
+                     value = bind 'moo_product',
+                     items = {
+                        {
+                           title = 'Business Cards',
+                           value = 'businesscard'
+                        }, 
+                        {
+                           title = 'MiniCards',
+                           value = 'minicard'
+                        }, 
+                        {
+                           title = 'Postcards',
+                           value = 'postcard'
+                        }
+                     }
+                  }
+               },
+               f:row {
+                  spacing = f:label_spacing(),
+                  f:static_text {
+                     title = "Cards will be",
+                     alignment = "right",
+		     width = LrView.share "label_width"
+                  },
+                  f:popup_menu {
+                     value = bind 'moo_orientation',
+                     items = {
+                        {
+                           title = 'landscape',
+                           value = 'landscape'
+                        }, 
+                        {
+                           title = 'portrait',
+                           value = 'portrait'
+                        }, 
+                        {
+                           title = 'oriented automatically',
+                           value = 'automatic'
+                        }
+                     }
+                  }
+               }
+            }
 	 }
       }
-   }
-   
+   }   
    return result
    
 end
